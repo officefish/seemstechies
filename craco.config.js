d4f80b5c044149a655e19ddd63d33fe126ad9061
inozemcev (windows) <= officefish>
2021-08-28 23:02:40 +0300

    postcss success setup

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+    style: {
+        postcss: {
+            mode: "extends" /* (default value) */ || "file",
+            plugins: [
+                require('tailwindcss')("./tailwind.config.js"),
+                require('autoprefixer')
+            ], // Additional plugins given in an array are appended to existing config.
 
+        }
+    }
 }
